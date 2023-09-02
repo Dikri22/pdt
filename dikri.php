babc4ff4124c2817423bba8add600001db6aaa1a
Dikri22 <89836977+Dikri22@users.noreply.github.com>
2023-09-02 21:31:23 +0700

    Add files via upload

---
+++ dikri.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>test PDT Dikri</title>
+</head>
+<body>
+<h1>HALLO NAMA SAYA DIKRI MAULANA</h1>
+</body>
+</html>
